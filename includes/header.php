10c8fe09bf25e7a31d77ca2e63ade3d5d5a8821d
Brandon Pack <bpack@adobe.com>
2020-02-02 12:01:27 -0700

    Changed the generic site to be html and php files for the 2020 Summit Lab

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<?php
+ // This file is included into the <head> tag on every page of the site.
+?>
+<!-- Site Meta Tags-->
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="keywords" content="travel, booking, hotels, adobe, vacation, summit">
+    <meta name="date" content="Mar 29">
+    <meta name="robots" content="noindex" />
+
+    <!-- Open Graph data -->
+    <meta property="og:title" content="We Travel Site" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="http://wetravel.com/" />
+    <meta property="og:image" content="http://wetravel.com/we_travel_logo.png" />
+    <meta property="og:description" content="Travel Booking Site" />
+
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+    <!-- Stylesheets-->
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=PT+Sans:400,400italic,700,700italic%7CMontserrat:400,700">
+    <link rel="stylesheet" href="css/style.css">
+    
+		<!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+		<![endif]-->
+
+    <!-- PASTE ADOBE LAUNCH EMBED CODE RIGHT BELOW THIS LINE -->
+
